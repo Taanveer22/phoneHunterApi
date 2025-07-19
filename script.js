a2dff8bc4bf2df3a45c1888cc59f67543edebcce
Taanveer22 <taanveer469@gmail.com>
2025-07-20 05:55:08 +0600

    alhamdulliah details button fetched from api

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // step 01 : load phones data from api-------------------------
-const loadPhones = async (status) => {
+const loadPhones = async (status, searchText) => {
   // step 08 : implement status into loadPhones()------------
-  console.log(status);
+  console.log(status, searchText);
   console.log("3 seconds later loadPhones function call...");
   // step 04 : hide the spinner------------------------------
   document.getElementById("spinner").style.display = "none";
   // step 05 : fetch data from api----------------------------
+  //   step 10: dynamic api call--------------------
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${
+      searchText ? searchText : "iphone"
+    }`
   );
   const data = await response.json();
   const phones = data.data;
@@ -24,10 +27,10 @@ const loadPhones = async (status) => {
 //step 06 : display phones data in the ui--------------------------
 const displayPhones = (phones) => {
   console.log(phones);
-  const phoneContainer = document.getElementById("phones-container");
+  const phonesContainer = document.getElementById("phones-container");
   phones.forEach((phone) => {
-    // destructuring object
     // console.log(phone);
+    // destructuring object
     const { brand, phone_name, slug, image } = phone;
     const phoneCard = document.createElement("div");
     phoneCard.innerHTML = `
@@ -37,28 +40,31 @@ const displayPhones = (phones) => {
                     </figure>
                     <div class="card-body items-center text-center">
                         <h2 class="card-title">${brand}</h2>
-                        <p>${phone_name}</p>
-                        <p>${slug}</p>
+                        <p class="text-lg">${phone_name}</p>
                         <div class="card-actions">
-                            <button class="btn btn-primary">
-                                    Show Details
+                            <button 
+                                    onclick = handleDetails('${slug}')
+                                    class="btn btn-primary">
+                                          Details
                             </button>
                         </div>
                     </div>
             </div>
         `;
-    phoneContainer.appendChild(phoneCard);
+    phonesContainer.appendChild(phoneCard);
   });
 };
 
 // step 02 : onclick handler for search button-----------------------------
 const handleSearch = () => {
   console.log("search button clicked");
+  //   step 11 : search function implementation-----------------
+  const searchValue = document.getElementById("search-box").value;
   //   step 04 : show the spinner------------------------
   document.getElementById("spinner").style.display = "block";
   //step 03 : invoke loadPhones() inside setTimeout-------------------------
   setTimeout(function () {
-    loadPhones();
+    loadPhones(false, searchValue);
   }, 3000);
 };
 
@@ -69,5 +75,15 @@ const handleShowAll = () => {
   loadPhones(true);
 };
 
+// step 12 : show modal for details button
+const handleDetails = async (slug) => {
+  console.log(slug);
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089"
+  );
+  // console.log(response);
+  const data = await response.json();
+  console.log(data.data.mainFeatures);
+};
 // final function invocation
-loadPhones();
+loadPhones(false, "iphone");
